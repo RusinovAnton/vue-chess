58793ccc3ecc9a643ca734951274b729c7e3d690
Anton Rusinov <rusinovantondev@gmail.com>
2020-10-18 20:59:07 +0300

    add basic components and data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  hi
+  <Board />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import Board from './components/Board.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
+    Board
+  }
 });
 </script>
 
